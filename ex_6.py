c48a171d0a0a0d395c0ee6942fa70a385a3e188a
dr.number <dr.number@yandex.ru>
2023-05-08 17:11:51 +0700

    add ex 6

--- ex_6.py
+++ ex_6.py
@@ -1,4 +1,6 @@
 from helpers import get_text_color, input_number, is_question, COLOR_GREEN, COLOR_FAIL, COLOR_WARNING
+_FORMAT = '| {:10} | {:30} | {:30} | {:30} |'
+
 _DEFAULT_STEP = 0.1
 _MIN_STEP = 0.1
 _MAX_STEP = 1
@@ -15,7 +17,7 @@ def init():
     _example = (
         f'     | x * 0.5,    если x < -1\n'
         f'y = <  0      ,    если x >= -1\n'
-        f'     | x      ,    если x >= 1\n'
+        f'     | x      ,    если x <= 1\n'
         f'     | 1      ,    если x > 1\n'
     )
     print(
@@ -43,3 +45,42 @@ def init():
         min=_MIN_END_X,
         max=_MAX_END_X
     )
+
+    print(get_text_color(_FORMAT.format(*[
+        '№',
+        'Выбор', 
+        'Формула', 
+        'результат'
+    ]), COLOR_WARNING))
+
+    x = start_x
+    i = 1
+    while start_x <= x <= end_x:
+
+        if x < -1:
+            select = f'Т.к. x < -1 -> {x} < -1 -> y = x * 0.5'
+            formul = f'{x} * 0.5'
+            result = x * 0.5
+        elif x >= -1:
+            select = f'Т.к. x >= -1 -> {x} >= -1 -> y = 0'
+            formul = '0'
+            result = '0'
+        elif x <= 1:
+            select = f'Т.к. x <= 1 -> {x} <= 1 -> y = x'
+            formul = f'x'
+            result = f'{x}'
+        elif x > 1:
+            select = f'Т.к. x > 1 -> {x} > 1 -> y = 1'
+            formul = f'1'
+            result = f'1'
+        else:
+            select = ''
+            formul = 'Не подходит под условие задачи!'
+            result = ''
+
+        print(get_text_color(_FORMAT.format(*[
+        f'{i}',
+        'Выбор', 
+        'Формула', 
+        'результат'
+    ]), COLOR_GREEN))
