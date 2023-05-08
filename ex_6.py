14ac0aecf3fd81aef66b6d9e1380a53ff8794be9
dr.number <dr.number@yandex.ru>
2023-05-08 17:24:22 +0700

    add ex 6

--- ex_6.py
+++ ex_6.py
@@ -1,5 +1,5 @@
-from helpers import get_text_color, input_number, is_question, COLOR_GREEN, COLOR_FAIL, COLOR_WARNING
-_FORMAT = '| {:10} | {:30} | {:30} | {:30} |'
+from helpers import get_text_color, input_number, COLOR_OKBLUE, COLOR_GREEN, COLOR_OKCYAN, COLOR_FAIL, COLOR_WARNING
+_FORMAT = '| {:10} | {:60} | {:35} | {:30} |'
 
 _DEFAULT_STEP = 0.1
 _MIN_STEP = 0.1
@@ -11,7 +11,7 @@ _DEFAULT_START_X = -2
 
 _MIN_END_X = 2
 _MAX_END_X = 20
-_DEFAULT_END_X = -2
+_DEFAULT_END_X = 2
 
 def init():
     _example = (
@@ -25,33 +25,33 @@ def init():
         f'{get_text_color(_example, COLOR_GREEN)}\n'        
     )
 
-    step = input_number(
+    step_x = input_number(
         text=f'Введите шаг: (по умолчанию {_DEFAULT_STEP})', 
-        default=_DEFAULT_STEP,
+        default_value=_DEFAULT_STEP,
         min=_MIN_STEP,
         max=_MAX_STEP
     )
 
     start_x = input_number(
         text=f'Введите начальное значение x: (по умолчанию {_DEFAULT_START_X})', 
-        default=_DEFAULT_START_X,
+        default_value=_DEFAULT_START_X,
         min=_MIN_START_X,
         max=_MAX_START_X
     )
 
     end_x = input_number(
         text=f'Введите конечное значение x: (по умолчанию {_DEFAULT_END_X})', 
-        default=_DEFAULT_END_X,
+        default_value=_DEFAULT_END_X,
         min=_MIN_END_X,
         max=_MAX_END_X
     )
 
-    print(get_text_color(_FORMAT.format(*[
+    print(_FORMAT.format(*[
         '№',
         'Выбор', 
         'Формула', 
         'результат'
-    ]), COLOR_WARNING))
+    ]))
 
     x = start_x
     i = 1
@@ -61,26 +61,34 @@ def init():
             select = f'Т.к. x < -1 -> {x} < -1 -> y = x * 0.5'
             formul = f'{x} * 0.5'
             result = x * 0.5
+            color = COLOR_OKCYAN
         elif x >= -1:
             select = f'Т.к. x >= -1 -> {x} >= -1 -> y = 0'
             formul = '0'
             result = '0'
+            color = COLOR_OKBLUE
         elif x <= 1:
             select = f'Т.к. x <= 1 -> {x} <= 1 -> y = x'
             formul = f'x'
             result = f'{x}'
+            color = COLOR_WARNING
         elif x > 1:
             select = f'Т.к. x > 1 -> {x} > 1 -> y = 1'
             formul = f'1'
             result = f'1'
+            color = COLOR_GREEN
         else:
             select = ''
             formul = 'Не подходит под условие задачи!'
             result = ''
+            color = COLOR_FAIL
 
         print(get_text_color(_FORMAT.format(*[
-        f'{i}',
-        'Выбор', 
-        'Формула', 
-        'результат'
-    ]), COLOR_GREEN))
+            f'{i}',
+            select, 
+            formul, 
+            result
+        ]), color))
+
+        x += step_x
+        i += 1
